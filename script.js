02696387f6c79b61638f7c95665881b9addc4260
ahmedxr12 <aa0505295@gmail.com>
2022-10-29 21:23:04 +0300

    classwork 7 completed

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function BMI (weight = 60, height = 1.6) 
+{
+    return weight / (height * height);
+}
 
+console.log(BMI(65, 1.8))
+console.log(BMI())
+
+
+function Status (BodyMass)
+{
+    if(BodyMass < 18.5) {
+        return "لديك نقص في الوزن"
+    }else if(BodyMass >= 18.5 && BodyMass < 25){
+        return "وزنك صحي"
+    }else{
+        return "لديك زيادة في الوزن"
+    }
+}
+
+function calculate () 
+{
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    let bmi_value = BMI(weight, height)
+    let desc = Status(bmi_value)
+    let box = document.getElementById('result')
+    box.innerText = bmi_value + " == " + desc
+}
